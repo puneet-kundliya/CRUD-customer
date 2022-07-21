3b71aae2ca907578e7fb35ffaefaaed4fe421762
Puneet Kundliya <puneet.kundliya@gmail.com>
2022-07-21 15:34:38 +0530

    changes in Repo

--- src/main/java/com/puneet/CustomerAPI/Repository/CustomerRepository.java
+++ src/main/java/com/puneet/CustomerAPI/Repository/CustomerRepository.java
@@ -2,10 +2,8 @@ package com.puneet.CustomerAPI.Repository;
 
 import com.puneet.CustomerAPI.Entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 
 @Repository
 public interface CustomerRepository extends JpaRepository< Customer, Integer> {
